b9bad1e4bd30cb6010610090b1717168ed7d7bb8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-04-10 11:12:12 -0300

    Add some documentation

--- Rasp.Test/Program.cs
+++ Rasp.Test/Program.cs
@@ -9,6 +9,56 @@ using System.Threading;
 using Telegram.Bot;
 using Telegram.Bot.Extensions.Polling;
 
+
+/*
+    Welcome to my Bot Demo Project!
+    I Hope you can learn a lot from it. 
+    If you have any sugestions about usability and learning, 
+     feel free to colaborate with the project
+
+    How to start?
+    1. You must talk with Telegram's bot: @BotFather to create your bot and get your token
+    2. Initialize this bot using command line arguments, and setting:
+      a. Your token
+      b. Your admin account (your account id)
+      The easier way to do that is in visual studio's project settings
+      But you can also uncomment down below
+    3. Run the project and interact with the bot
+      You can see the available commands in Commands.cs and HelpCommands.cs
+
+    This project has a lot of built-in tools
+    1. Configuration Manager
+    2. DI Engine (Dependecy Injection - a simple and lightweight one)
+    3. Built-in logs
+    4. A versatile Task Scheduler (see TelegramPing.cs)
+    5. All commands use an Endpoint-like mechanism 
+       You can add commands simply adding a new "Route" to the Endpoint
+
+
+    How can I add a new commands?
+     All classes with commands are added in the `setupTelegramCommands` method
+     Simply add a new Controller like the existing ones
+     All controllers (classes) addded have it's methods added
+
+    How can I add a new Scheduler?
+     1. Create a new JobBase
+     2. Setup it's triggers in the constructor (see TelegramPing.cs)
+     3. Overrides the ExecuteAsync method
+     4. Add the class to the Scheduler (method `AddJobs` below)
+
+    So ... This bot has DI? How do I uset it?
+     Call `Injector.Get<TheClassYouWant>()` 
+     For example, you can get the Logger instance with: Injector.Get<ILogger>()
+     • var cfg = Injector.Get<Config>(); // To get the config
+     • var bot = Injector.Get<TelegramBotClient>(); // To get the TelegramBotClient
+     And you can add your class with Injector.AddSingleton<MyClass>(myClassInstance);
+
+    This bot-demo is OpenSource and available at https://github.com/RafaelEstevamReis/BotEngine-Demo
+    The underlying library `BotUtils` is OpenSource and available at https://github.com/RafaelEstevamReis/Simple.BotUtils
+
+ */
+
+
 namespace Rasp.Test
 {
     class MainClass
@@ -18,6 +68,16 @@ namespace Rasp.Test
             Console.WriteLine("Initializing... v0.1");
 
             var cancellationSource = new CancellationTokenSource();
+
+            // Uncomment this part to setup the bot, you have to do this only once
+            // BUT NEVER EVER commit your code with your token
+
+            /*
+                // Ask for the token 
+                Console.WriteLine("Paste your @BotFather token");
+                string token = Console.ReadLine();
+                args = new string[] { "-token", token };
+            */
 
             setupConfig(args);
             setupLogs();
@@ -72,7 +132,16 @@ namespace Rasp.Test
             var cfg = Injector.Get<Config>();
 
             if (cfg.TelegramToken == null) throw new Exception("Telgram bot not configured");
-            if (cfg.TelegramAdmin == 0) throw new Exception("Telgram admin not configured");
+            if (!cfg.TelegramToken.Contains(":")) throw new Exception("Invalid Token");
+            if (cfg.TelegramAdmin == 0)
+            {
+                // Configure here if you want to setup a an admin
+                // Uncomment below to not let you bot start without an admin setted
+                //throw new Exception("Telgram admin not configured");
+
+                // You can later use this to decide if someone is Worthy of accessing your bot
+                Console.WriteLine("No Admin configured !!");
+            }
 
             var client = new TelegramBotClient(cfg.TelegramToken);
             client.StartReceiving(new DefaultUpdateHandler(UpdateHandler.HandleUpdateAsync, UpdateHandler.HandleErrorAsync), cancellationToken: token);
